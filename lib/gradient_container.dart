ff9f90a889f27ec22f841bb98eab6184c77dda5e
goeunrain <boniteterain@gmail.com>
2023-06-24 23:44:20 +0900

    feat : splitting code across files

---
+++ lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 117, 94, 148)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: const Center(
+        child: Text('Hello World!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            )),
+      ),
+    );
+  }
+}
